99ef7d1c33e80b709585c09919fc9d6b82338db4
marie charlotte <charlottedoulcet90@gmail.com>
2026-01-10 18:05:45 +0100

    feat(api) add try/catch and error logging to API calls getAllCards and getCardByID. refs: TS-32

--- src/services/sakuraApi.jsx
+++ src/services/sakuraApi.jsx
@@ -4,14 +4,25 @@ export const sakuraApi = () => {
   const url = "https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards";
 
   const getAllCards = async () => {
-    const response = await axios.get(url);
-    return response.data;
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.error("Error en getAllCards:", error.message);
+      throw error;
+    }
   };
 
   const getCardById = async (id) => {
-    const response = await axios.get(`${url}/${id}`);
-    return response.data;
+    try {
+      const response = await axios.get(`${url}/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error en getCardById:", error.message);
+      throw error;
+    }
   };
+
   return {
     getAllCards,
     getCardById,
