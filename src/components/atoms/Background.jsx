300312ba4776edcc4967aa3612e015c395c22253
ManonChab <godfroymanon@gmail.com>
2026-01-12 11:58:14 +0000

    feat Background without style

---
+++ src/components/atoms/Background.jsx
@@ -0,0 +1,11 @@
+import tailwindcss from "@tailwindcss/vite"
+
+
+export const Background = () => {
+    return (
+        <div>
+            <img src="../src/assets/images/Background.png" alt="pink sakura background"
+            className="z-0 w-100% h-100% " />
+        </div>
+    )
+}
