160153b21d0758bf2016a8a370a504cbc72cf57c
ManonChab <godfroymanon@gmail.com>
2026-01-13 18:15:55 +0000

    HomePage ok with design

---
+++ src/components/atoms/HistoryIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const HistoryIcon = () => {
+  return (
+    <div className='z-1'>
+        <a>
+            <img src="../src/assets/images/HistoryIcon.svg" alt="Book Icon" 
+                className='size-20 '></img>
+        </a>
+    </div>
+  )
+}
---
+++ src/components/atoms/ReadingIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ReadingIcon = () => {
+  return (
+    <div className='z-1'>
+        <a href="">
+            <img src="../src/assets/images/ReadingsIcon.svg"
+                className='size-20'></img>
+        </a>
+    </div>
+  )
+}
