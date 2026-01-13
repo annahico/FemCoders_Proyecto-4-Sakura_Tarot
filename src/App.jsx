fd56aa0430c5f65842cf7a3b737b074cd03592fb
ManonChab <godfroymanon@gmail.com>
2026-01-13 18:39:29 +0000

    feat added name to the Home icons

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { HomePage } from "./pages/HomePage";
 export const App = () => {
   return (
     <div>
-
+      <HomePage></HomePage>
     </div>
   )
 }
--- src/components/atoms/HistoryIcon.jsx
+++ src/components/atoms/HistoryIcon.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 export const HistoryIcon = () => {
   return (
-    <div className='z-1'>
-        <a>
+    <div className='z-1 pl-20'>
+        <a className='flex flex-col items-center'>
             <img src="../src/assets/images/HistoryIcon.svg" alt="Book Icon" 
-                className='size-20 '></img>
+                className='size-20'></img>
+            <p className='font-[monserrat] text-[#6A4A4A] p-5'>Historial de Lecturas</p>
         </a>
     </div>
   )
