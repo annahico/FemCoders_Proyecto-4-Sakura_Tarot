28018b7dffa6f8b63709f899fa9d869207895fb5
annahico <annahico@gmail.com>
2026-01-14 09:44:44 +0100

    feat: removed comments

--- src/components/molecules/CardSelection.jsx
+++ src/components/molecules/CardSelection.jsx
@@ -5,7 +5,6 @@ import { Cards } from '../atoms/Cards'
 export const CardSelection = () => {
   return (
     <div>
-        {/* <CardBack></CardBack> */}
         <div className='z-2 bg-amber-600 bg-opacity-100 flex'>
             <div className='z-3 grid grid-cols-5 grid-row-2 gap-y-10 col-g w-4xl items-center'>
                 <Cards></Cards>
--- src/context/TarotContext.jsx
+++ src/context/TarotContext.jsx
@@ -1,9 +1,6 @@
 import { createContext, useContext } from 'react';
 
-// Creamos el espacio de memoria
 export const TarotContext = createContext();
-
-// Creamos el "gancho" para que los componentes accedan a los datos
 export const useTarot = () => {
   const context = useContext(TarotContext);
   if (!context) {
